ccf6a4ca0c17e4ed82563d1d4c2056900ec88275
Jamal <96631438+Jamal-daboul@users.noreply.github.com>
2025-02-08 00:46:37 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,1 @@
-function displayImage(base64Image) {
-    imageContainer.innerHTML = ''; // Clears current image
-    const imgElement = document.createElement('img');
-    imgElement.src = `data:image/jpeg;base64,${base64Image}`;
-    imageContainer.appendChild(imgElement); // Appends new image
-}
+import { getFirestore, collection, addDoc, getDocs } from "firebase/firestore";
